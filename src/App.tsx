5a9fd2a039022fa96b0906713f0f391c4a11fc98
M. Thaler <mats68@gmail.com>
2024-11-03 20:40:37 +0100

    icon

--- src/App.tsx
+++ src/App.tsx
@@ -127,7 +127,7 @@ const App: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold mb-8">Roulette Table</h1>
+      <h1 className="text-3xl font-bold mb-8">Roulette-Assistent</h1>
 
       {!showForm && !showTable ? (
         <ModeButtons onSelectMode={handleSelectMode} />
